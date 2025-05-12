2c4fe2ca019ee81371349aefde22b3e6dbb79692
joelparkeruofa <joelparker@joels-air.mynetworksettings.com>
2025-05-12 11:47:34 -0700

    adding subpathay analysis, pairwise analysis, and subpathway analysis plots

---
+++ app/ui-tab-subpath_analysis.R
@@ -0,0 +1,16 @@
+fluidRow(
+  column(3,
+         box(label = "Controls",
+             width = 12,
+             collapsible = FALSE,
+             closable = FALSE,
+             
+             selectInput('groupVars', "Column annotation variables",
+                         choices = NULL, multiple = TRUE),
+             selectInput('stratVar', "Variable to stratifiy the plots by",
+                         choices = NULL, selected = NULL),
+             numericInput("metsNumber", "Number of Metabolites to include", 
+                          min = 1, value = 50)
+             
+         ))
+)
--- app/ui.R
+++ app/ui.R
@@ -22,14 +22,17 @@ dashboardPage(
       menuItem("Exploratory Analysis", tabName = "exploratoryAnalysis",
                icon = icon("wrench"),
                menuSubItem("PCA",tabName = "pca", icon = icon("figma")),
-               menuSubItem("Heatmaps",tabName = "heatmaps", icon = icon("stroopwafel"))),
+               menuSubItem("Heatmaps", tabName = "heatmaps",
+                           icon = icon("stroopwafel"))),
       
       menuItem("Subpathway Analysis", tabName = "subpathAnalysis",
                icon = icon("pen-nib")),
       menuItem("Pairwise Analysis", tabName = "pairwiseAnalysis",
                icon = icon("brush")),
       menuItem("Subpathway Plots", tabName = "subpathwayPlots",
-               icon = icon("lines-leaning"))
+               icon = icon("lines-leaning"),
+               menuSubItem("Subpathway Box Plots", tabName = "subpathBox"),
+               menuSubItem("Subpathway Line Plots", tabName = "subpathLine"))
       
     ),
     
@@ -47,7 +50,20 @@ dashboardPage(
               source("ui-tab-pca.R", local=TRUE)$value), 
       
       tabItem("heatmaps", 
-              source("ui-tab-heatmap.R", local = TRUE)$value)
+              source("ui-tab-heatmap.R", local = TRUE)$value),
+
+      tabItem("subpathwayAnalysis",
+              source("ui-tab-subpath_analysis.R", local = TRUE)$value),
+
+      tabItem("pairwiseAnalysis",
+              source("ui-tab-pairwiseAnalysis.R", local = TRUE)$value),
+      
+      
+      tabItem("subpathBox",
+              source("ui-tab-subpathway_boxplots.R", local = TRUE)$value),
+      
+      tabItem("subpathLine",
+              source("ui-tab-subpathway_Lineplots.R", local = TRUE)$value)
       
     )
     
